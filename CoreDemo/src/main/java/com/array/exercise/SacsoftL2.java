95e77c8e3f1d793a790c00622008ba58e9443107
komalkorane <komalkorane2206@gmail.com>
2023-07-11 12:16:06 +0530

    "new commit"

--- CoreDemo/src/main/java/com/array/exercise/SacsoftL2.java
+++ CoreDemo/src/main/java/com/array/exercise/SacsoftL2.java
@@ -1,9 +1,9 @@
 package com.array.exercise;
 
 public class SacsoftL2 {
-// static int t=7;
+ static int t=7;
 	public static void main(String[] args) {
-//		System.out.println("one"+t);
+		System.out.println("one"+t);
 		for(int i=0; i<=10;i++) {
 			if(i>6)
 				break;
--- CoreDemo/src/main/java/com/array/exercise/TMF3.java
+++ CoreDemo/src/main/java/com/array/exercise/TMF3.java
@@ -1,19 +1,27 @@
 package com.array.exercise;
+
 class Driver {
 	int num;
+
 	public Driver(int num) {
-//		num = num;
+		num = num;
 	}
+
 	public Driver() {
 		num = 0;
-}}
+	}
+}
+
 public class TMF3 {
 	public static void main(String[] args) {
 		Driver t = new Driver(20);
 		update(t);
 		System.out.println(t.num);
 	}
+
 	public static void update(Driver o) {
 		o = new Driver();
+		System.out.println(o.num);
 		o.num = 50;
-	}}
+	}
+}
